0e5d3640714ad0ad5e8cf848514cfef792909c1f
moklick <info@moritzklack.com>
2016-02-16 18:12:11 +0100

    remove fetch

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,6 +12,4 @@ import '../styles/index.scss';
 import dataSrc from 'file?name=[path][name].[ext]!../data/data.json';
 
 $.getJSON(dataSrc)
-  .then((res) => {
-    console.log(res);
-  });
+  .then(res => console.log(res));
